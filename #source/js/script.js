2bbf43519e8d53d4c830cc2f387b9f018d799e1c
hron2017 <andreypbiz@gmail.com>
2022-03-09 14:01:47 +0300

    comit

--- #source/js/script.js
+++ #source/js/script.js
@@ -1,3 +1,19 @@
+//====================  <!-- GoTO -->  ========================//
+const links = document.querySelectorAll('.scroll-to');
+for (let i = 0; i < links.length; i++) {
+   links[i].addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const blockId = e.target.getAttribute('href').substring(1);
+
+      document.getElementById(blockId).scrollIntoView({
+         behavior: 'smooth',
+         block: 'start'
+      });
+   });
+}
+//====================  <!-- GoTO -->  ========================//
+
 //==================== <!-- ImageBg --> ========================//
 function ibg() {
    let ibg = document.querySelectorAll(".ibg");
@@ -61,3 +77,79 @@ const reviewsSwiper = new Swiper('.slider-reviews', {
       }
    }
 });
+
+
+/*        intlTelInput         */
+
+const inputPhone = document.querySelector("#phone"),
+   errorMsg = document.querySelector("#error-msg"),
+   validMsg = document.querySelector("#valid-msg");
+
+var errorMap = ["Invalid number", "Invalid country code", "Too short", "Too long", "Invalid number"];
+
+const intl = window.intlTelInput(inputPhone, {
+   initialCountry: 'ua',
+   utilsScript: "js/utils.js"
+});
+
+const reset = function () {
+   inputPhone.classList.remove("error");
+   errorMsg.innerHTML = "";
+   errorMsg.classList.add("hide");
+   validMsg.classList.add("hide");
+};
+
+inputPhone.addEventListener('blur', function () {
+   reset();
+   if (inputPhone.value.trim()) {
+      if (intl.isValidNumber()) {
+         validMsg.classList.remove("hide");
+      } else {
+         inputPhone.classList.add("error");
+         var errorCode = intl.getValidationError();
+         errorMsg.innerHTML = errorMap[errorCode];
+         errorMsg.classList.remove("hide");
+      }
+   }
+});
+
+inputPhone.addEventListener('change', reset);
+inputPhone.addEventListener('keyup', reset);
+
+/*        //intlTelInput         */
+/*
+const button = document.querySelector('#form-button'),
+   input = document.querySelector('input'),
+   result = document.querySelector('#input-error');
+
+
+
+button.addEventListener('click', function () {
+
+   if (input.value === "") {
+      button.setAttribute('disabled', true);
+      result.textContent = "Заповніть пусте поле";
+   } else {
+      result.textContent = "Все ок!";
+      button.removeAttribute('disabled', false);
+   }
+}, false)
+
+
+function checkParams() {
+   var name = $('#name').val();
+   var email = $('#email').val();
+   var phone = $('#phone').val();
+
+   if (name.length != 0 && email.length != 0 && phone.length != 0) {
+      $('#submit').removeAttr('disabled');
+   } else {
+      $('#submit').attr('disabled', 'disabled');
+   }
+}
+
+$('#personaldata').change(function () {
+   $('#submit').prop("disabled", !this.checked);
+}).change()*/
+
+
